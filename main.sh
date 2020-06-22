5ad07582cea053660919ca3fd8de37d082a8a0c0
sudevschiz <sudevschiz@bots.github.com>
2020-06-23 01:09:18 +0900

    Enable push

--- main.sh
+++ main.sh
@@ -61,18 +61,18 @@ rm -r tmp/
 
 
 
-# git add .
-# set +e  # Grep succeeds with nonzero exit codes to show results.
-# git status | grep 'new file\|modified'
-# if [ $? -eq 0 ]
-# then
-#     set -e
-#     git commit -am "data updated on - $(date)"
-#     git remote set-url "$remote_name" "$repo_uri" # includes access token
-#     git push --force-with-lease "$remote_name" "$gh_pages_branch"
-# else
-#     set -e
-#     echo "No changes since last run"
-# fi
+git add .
+set +e  # Grep succeeds with nonzero exit codes to show results.
+git status | grep 'new file\|modified'
+if [ $? -eq 0 ]
+then
+    set -e
+    git commit -am "data updated on - $(date)"
+    git remote set-url "$remote_name" "$repo_uri" # includes access token
+    git push --force-with-lease "$remote_name" "$gh_pages_branch"
+else
+    set -e
+    echo "No changes since last run"
+fi
 
 echo "finish"
