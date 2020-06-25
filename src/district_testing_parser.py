0b20694e1dc071d0075047f902d58dea47430766
Sudev <sudevschiz@users.noreply.github.com>
2020-06-26 03:11:27 +0900

    Change NaNs to empty string

--- src/district_testing_parser.py
+++ src/district_testing_parser.py
@@ -15,10 +15,12 @@ def make_int_if_possible(c):
     csv has integer values as strings.
     Convert them to integer if possible
     """
+    if isinstance(c, str):
+        return c
     try:
         return int(c)
     except ValueError:
-        return c
+        return ""
 
 
 def parse_testing(x):
@@ -69,8 +71,8 @@ if __name__ == "__main__":
     for i in range(1, len(ind_di)):
         dist_testing.append(parse_testing(ind_di[i]))
     logging.info(f"{len(dist_testing)} districts processed")
-    logging.info("Save district_test_data.json\n------------")
-    dist_testing_dict = {"district_test_data": dist_testing}
-    with open(Path("tmp", "district_test_data.json"), "w") as js:
-        json.dump(dist_testing_dict, js, indent=4)
+    ddt = {"district_testing": dist_testing}
+    logging.info("Save district_testing.json\n------------")
+    with open(Path("tmp", "district_testing.json"), "w") as js:
+        json.dump(ddt, js)
 
