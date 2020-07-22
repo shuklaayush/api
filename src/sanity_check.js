7d5174a2d5e6d76f68ec9f1c28aba323d5a4fcc7
Bharath <bee.ricketyship@gmail.com>
2020-07-22 22:40:17 +0530

    Increasing the delta limit to 47k :(

--- src/sanity_check.js
+++ src/sanity_check.js
@@ -2,8 +2,8 @@ const data = require('../tmp/data.json')
 
 var total = data.statewise[0]
 var err = false
-if (total.deltaconfirmed > 45000) {
-  console.error('Delta confirmed is greater than 45k')
+if (total.deltaconfirmed > 47000) {
+  console.error('Delta confirmed is greater than 47k')
   err = true
 }
 
