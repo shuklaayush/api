6654fa30ebaa66fa9d455d9dce18dea354865c23
sudevschiz <sudevschiz@gmail.com>
2020-11-13 16:38:23 +0900

    Run all versions

--- src/sheet-to-json_generic.js
+++ src/sheet-to-json_generic.js
@@ -3,118 +3,118 @@ const c = require('../lib/constants');
 
 (async function main() {
   console.log('Running task on start...')
-  // await task({
-  //   sheet: c.SHEET_LOCALE,
-  //   tabs: { locales: 'od6' },
-  //   file: '/locales.json'
-  // })
+  await task({
+    sheet: c.SHEET_LOCALE,
+    tabs: { locales: 'od6' },
+    file: '/locales.json'
+  })
 
-  // await task({
-  //   sheet: c.SHEET_LOCALE,
-  //   tabs: { locales_progress: 'ou6ga5q' },
-  //   file: '/locales_progress.json'
-  // })
+  await task({
+    sheet: c.SHEET_LOCALE,
+    tabs: { locales_progress: 'ou6ga5q' },
+    file: '/locales_progress.json'
+  })
 
-  //   // uncomment below if v1 sheet has updates
-  // await task({
-  //   sheet: c.SHEET_v1,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_1
-  // })
-  // await task({
-  //   sheet: c.SHEET_v2,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_2
-  // })
-  // await task({
-  //   sheet: c.SHEET_v3,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_3
-  // })
+    // uncomment below if v1 sheet has updates
+  await task({
+    sheet: c.SHEET_v1,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_1
+  })
+  await task({
+    sheet: c.SHEET_v2,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_2
+  })
+  await task({
+    sheet: c.SHEET_v3,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_3
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v4,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_4
-  // })
+  await task({
+    sheet: c.SHEET_v4,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_4
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v5,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_5
-  // })
+  await task({
+    sheet: c.SHEET_v5,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_5
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v6,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_6
-  // })
+  await task({
+    sheet: c.SHEET_v6,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_6
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v7,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_7
-  // })
+  await task({
+    sheet: c.SHEET_v7,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_7
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v8,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_8
-  // })
+  await task({
+    sheet: c.SHEET_v8,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_8
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v9,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_9
-  // })
+  await task({
+    sheet: c.SHEET_v9,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_9
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v10,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_10
-  // })
+  await task({
+    sheet: c.SHEET_v10,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_10
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v11,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_11
-  // })
+  await task({
+    sheet: c.SHEET_v11,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_11
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v12,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_12
-  // })
+  await task({
+    sheet: c.SHEET_v12,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_12
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v13,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_13
-  // })
+  await task({
+    sheet: c.SHEET_v13,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_13
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v14,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_14
-  // })
+  await task({
+    sheet: c.SHEET_v14,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_14
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v15,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_15
-  // })
+  await task({
+    sheet: c.SHEET_v15,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_15
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v16,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_16
-  // })
+  await task({
+    sheet: c.SHEET_v16,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_16
+  })
   
-  // await task({
-  //   sheet: c.SHEET_v17,
-  //   tabs: { raw_data: c.SHEET_RAW_DATA },
-  //   file: c.FILE_RAW_DATA_17
-  // })  
+  await task({
+    sheet: c.SHEET_v17,
+    tabs: { raw_data: c.SHEET_RAW_DATA },
+    file: c.FILE_RAW_DATA_17
+  })  
 
   await task({
     sheet: c.SHEET_v18,
@@ -129,17 +129,17 @@ const c = require('../lib/constants');
   })  
 
   // uncomment below if v1 sheet has updates
-  // await task({
-  //   sheet: c.SHEET_v1,
-  //   tabs: { deaths_recoveries: c.SHEET_DEATHS_AND_RECOVERIES },
-  //   file: c.FILE_DEATHS_RECOVERIES_1
-  // })
+  await task({
+    sheet: c.SHEET_v1,
+    tabs: { deaths_recoveries: c.SHEET_DEATHS_AND_RECOVERIES },
+    file: c.FILE_DEATHS_RECOVERIES_1
+  })
 
-  // await task({
-  //   sheet: c.SHEET_v2,
-  //   tabs: { deaths_recoveries: c.SHEET_DEATHS_AND_RECOVERIES },
-  //   file: c.FILE_DEATHS_RECOVERIES_2
-  // })
+  await task({
+    sheet: c.SHEET_v2,
+    tabs: { deaths_recoveries: c.SHEET_DEATHS_AND_RECOVERIES },
+    file: c.FILE_DEATHS_RECOVERIES_2
+  })
 
   await task({
     sheet: c.SHEET,
@@ -158,21 +158,21 @@ const c = require('../lib/constants');
     file: c.FILE_DISTRICTS
   })
 
-  // await task({
-  //   sheet: c.SHEET,
-  //   tabs: {
-  //     travel_history: c.SHEET_TRAVEL_HISTORY
-  //   },
-  //   file: c.FILE_TRAVEL_HISTORY
-  // });
+  await task({
+    sheet: c.SHEET,
+    tabs: {
+      travel_history: c.SHEET_TRAVEL_HISTORY
+    },
+    file: c.FILE_TRAVEL_HISTORY
+  });
 
-  // await task({
-  //   sheet: c.SHEET,
-  //   tabs: {
-  //     factoids: c.SHEET_NAME_FACTOIDS, faq: c.SHEET_FAQ
-  //   },
-  //   file: c.FILE_WEBSITE_DATA
-  // })
+  await task({
+    sheet: c.SHEET,
+    tabs: {
+      factoids: c.SHEET_NAME_FACTOIDS, faq: c.SHEET_FAQ
+    },
+    file: c.FILE_WEBSITE_DATA
+  })
 
   // need to remove objects with empty states or empty totaltested
   var data = await fetchData({
@@ -210,17 +210,17 @@ const c = require('../lib/constants');
     file: c.FILE_SOURCES_LIST
   })
 
-  // await task({
-  //   sheet: c.SHEET_RESOURCES,
-  //   tabs: { resources: c.SHEET_RESOURCES_SHEET },
-  //   file: c.FILE_RESOURCES_ESSENTIALS
-  // })
+  await task({
+    sheet: c.SHEET_RESOURCES,
+    tabs: { resources: c.SHEET_RESOURCES_SHEET },
+    file: c.FILE_RESOURCES_ESSENTIALS
+  })
 
-  //   await task({
-  //     sheet: c.SHEET,
-  //     tabs: { zones: c.SHEET_ZONES },
-  //     file: c.FILE_ZONES
-  //   })
+    await task({
+      sheet: c.SHEET,
+      tabs: { zones: c.SHEET_ZONES },
+      file: c.FILE_ZONES
+    })
 
   await task({
     sheet: c.SHEET,
