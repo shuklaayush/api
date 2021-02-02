44052ba91f0c3e87477391b7e218cc0ff5915022
Dave <sudevschiz@gmail.com>
2021-02-02 22:45:35 +0900

    Run latest alone

--- main.sh
+++ main.sh
@@ -20,7 +20,7 @@ mkdir tmp/v2
 mkdir tmp/updatelog
 
 cp ./data.json ./tmp/data_prev.json
-# cp ./raw_data*.json ./tmp
+cp ./raw_data*.json ./tmp
 cp ./deaths_recoveries*.json ./tmp
 cp ./locales*.json ./tmp
 cp ./states_daily.json ./tmp
